c73e5cdf412bfad299302d18626af3994fbfa334
ram-izaap <ram.izaap@gmail.com>
2019-05-16 09:39:13 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './pages/home/home.module#HomeModule',
+    pathMatch: 'full'
+  },
+  {
+    path: 'category',
+    loadChildren: './pages/category/category.module#CategoryModule'
+  },
+  {
+    path: 'product',
+    loadChildren: './pages/product/product.module#ProductModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
